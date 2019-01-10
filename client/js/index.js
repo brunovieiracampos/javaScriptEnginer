836455f8c6560fa60e14bc1b197313e59eb04b2b
Bruno Vieira Campos <brunovieiracampos@mbp-de-bruno.home>
2019-01-09 22:31:20 -0200

    td dynamic

--- client/js/index.js
+++ client/js/index.js
@@ -7,5 +7,15 @@ var campos = [
 console.log(campos);
 
 document.querySelector('.form').addEventListener('submit', function(event) {
-    alert('oi');
+    var tr = document.createElement('tr');
+
+    campos.forEach(campo => {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume); 
 });
